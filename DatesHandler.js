dc149cefcf013b7a4163084175be0ef4a958a26b
Riduan <45457619+Rvnw@users.noreply.github.com>
2020-01-29 15:55:25 +0100

    Update DatesHandler.js
    naam toegevoegd

--- DatesHandler.js
+++ DatesHandler.js
@@ -1,3 +1,4 @@
+// developed by: Riduan van Noordt Wieringa
 $(document).ready(function () {
     const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
