df9b2f3a3cb7012083392e406494ccd94d483105
Riduan <45457619+Rvnw@users.noreply.github.com>
2020-01-29 15:52:53 +0100

    update.js toegevoegd
    script voor het tonen van live data, moet nog aangepast worden.

---
+++ update.js
@@ -0,0 +1,37 @@
+function loopToday() {
+    var interval = setInterval(function () {
+        updateToday();
+    }, 10000);
+}
+
+function updateToday() {
+    var url = 'http://localhost/project22/data/' + currentStationID + ".txt";
+    $.ajax({
+        type: 'GET',
+        url: url,
+        dataType: 'text',
+        success: function (data) {
+            let dayContainer = $('.dayContainer');
+            let todayBlock = $(dayContainer).children()[0];
+            let tempField = $(todayBlock).children()[3];
+            let rainField = $(todayBlock).children()[4];
+            let splitData = data.split("\n");
+            var latestIndex = splitData.length - 1;
+            let lastRecord = splitData[latestIndex];
+            while(lastRecord.length < 1){
+                latestIndex--;
+                lastRecord = splitData[latestIndex];
+            }
+            let dataArray = lastRecord.split(",");
+            let temp = dataArray[3];
+            let rain = dataArray[9];
+
+            console.log("TEMP:" + temp);
+            $(tempField).html("Temperature: " + temp + " F");
+            $(rainField).html("Rain: " + (rain * 10) + "mm/s");
+
+
+
+        }
+    });
+}
